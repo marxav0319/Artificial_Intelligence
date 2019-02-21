2e970925af11db486a502e40a41186606a6b11ba
Mark <mark.j.xavier@gmail.com>
2019-02-21 17:32:20 -0500

    Added some docstrings

--- Programming_Assignments/01_Search_and_Hill_Climbing/state.py
+++ Programming_Assignments/01_Search_and_Hill_Climbing/state.py
@@ -5,6 +5,13 @@ class Processor:
 
     This class serves as a wrapper for the processor speeds given in the input.  This is mainly
     created to ease printing.
+
+    Args:
+        index <int>: The processor index is simply a unique record id, start at 0
+        speed <int>: The speed of the processor, obtained from a text file
+
+    Returns:
+        <state.Processor>: An object initialised with the given arguments
     """
 
     def __init__(self, index, speed):
@@ -12,6 +19,8 @@ class Processor:
         self.speed = speed
 
     def __str__(self):
+        """ A convenience method for easy printing during debugging"""
+
         return "Processor " + str(self.index) + " Speed: " + str(self.speed)
 
 
@@ -21,6 +30,14 @@ class Task:
 
     This class serves as a wrapper for the task lengths given, and provides an interface for
     assigning tasks to processors.
+
+    Args:
+        index <int>: The task index is simply a unique record id, start at 0.
+        length <int>: The length of time for the task - used to calculate whether the deadline has
+                      been reached after processor assignment.
+
+    Returns:
+        <state.Task>: An object initialised with the given arguments
     """
 
     def __init__(self, index, length):
@@ -40,11 +57,33 @@ class Task:
         return self.length < other.length
 
     def __str__(self):
+        """ A convenience method for easy printing during debugging"""
+
         return ("Task " + str(self.index) + " Length: " + str(self.length)
                 + " Assigned to " + str(self.processor))
 
 
 class State:
+    """
+    The State Class
+
+    Holds a state (a node really) encapsulating all current processors, tasks, task assignments,
+    and parameters for deadlines and target lengths.  A new state can be created with data from an
+    existing state, and upon creation a new state will automatically compute whether or not it is
+    the goal state, along with any parameters necessary to determine if the goal state has been
+    attained.
+
+    Args:
+        processors <list <state.Processor> >: A list of processors given by the input text file
+        tasks <list <state.Task> >: A list of tasks given by the input text file
+        deadline <int>: The deadline for the processors to finish their assigned tasks
+        target <int>: The target is the minimum summed length of all assigned tasks in order for
+                      the goal state to be reached.
+
+    Returns:
+        <state.State>: An object initialised with the given arguments with an auto-calculated data
+                       member determining of the goal_state has been reached.
+    """
 
     def __init__(self, processors, tasks, deadline, target):
         self.processors = processors
@@ -127,6 +166,10 @@ class State:
         return self.assignments
 
     def __str__(self):
+        """
+        The built in string method so you can call "print <State>".  This was used for debugging
+        purposes but was left in just for completion.
+        """
         print_str = "\nProcessors\n"
 
         for processor in self.processors:
@@ -139,5 +182,11 @@ class State:
 
         print_str += "\nParameters\n"
         print_str += ("Deadline: " + str(self.deadline) + "\nTarget: " + str(self.target))
+
+        print_str += "\n\nTask Assignments At This State\n"
+        print_str += self.get_task_assignments()
+
+        print_str += "\n\nHas The Goal State Been Reached: " 
+        print_str += "Yes" if self.goal_state == True else "No"
 
         return print_str
