61f24d92979ca08c8144cda61b7910b89416f40d
Mark <mark.j.xavier@gmail.com>
2019-02-21 11:15:48 -0500

    Removed helpers.py
    It was unnecessary, state.py now contains all methods needed for
    storing, reading, loading, and updating states.

--- Programming_Assignments/01_Search_and_Hill_Climbing/iterative_deepening.py
+++ Programming_Assignments/01_Search_and_Hill_Climbing/iterative_deepening.py
@@ -2,13 +2,13 @@
 import sys
 import argparse
 
-from helpers import load_data
+from state import State
 
 def iterative_deepening(file_path):
     """
     """
 
-    initial_state = load_data(file_path)
+    initial_state = State.load_from_file(file_path)
     print initial_state
 
 def main():
--- Programming_Assignments/01_Search_and_Hill_Climbing/state.py
+++ Programming_Assignments/01_Search_and_Hill_Climbing/state.py
@@ -6,22 +6,27 @@ class Processor:
         self.speed = speed
 
     def __str__(self):
-        return "Processor " + str(self.index) + " Speed: " + str(self.speed)
+        return "Processor " + str(self.index) + " Speed: " + str(self.speed) + "\n"
 
 
 class Task:
 
-    def __init__(self, length):
+    def __init__(self, index, length):
+        self.index = index
         self.length = length
         self.processor = None
 
-    def assign_processor(processor):
+    def assign_processor(self, processor):
         self.processor = processor
         self.time_taken = self.length / self.processor.speed
 
-    def evaluate_task_time():
+    def evaluate_task_time(self):
         if(self.processor != None):
             return self.time_taken
+
+    def __str__(self):
+        return ("Task " + str(self.index) + " Length: " + str(self.length)
+                + " Assigned to " + str(self.processor) + "\n")
 
 
 class State:
@@ -33,51 +38,75 @@ class State:
         self.target = target
         self.goal_state = False
         self.processor_time_taken = None
+        self.compute_state()
+        self.evaluate_state()
+
+    @staticmethod
+    def read_file(file_path):
+        file_handle = open(file_path)
+        inputs = []
+        for i in xrange(3):
+            temp = file_handle.readline().strip().split(" ")
+            inputs.append([float(element) for element in temp])
+        file_handle.close()
+        return inputs
+
+    @classmethod
+    def load_from_file(cls, file_path):
+        task_lengths, processor_speeds, parameters = State.read_file(file_path)
+        tasks = []
+        processors = []
+        for index, length in enumerate(task_lengths):
+            tasks.append(Task(index, length))
+
+        for index, speed in enumerate(processor_speeds):
+            processors.append(Processor(index, speed))
+        
+        return cls(processors, tasks, parameters[0], parameters[1])
     
-    def compute_state():
+    def compute_state(self):
         self.processor_time_taken = [0 for i in xrange(len(self.processors))]
         self.target_progress = 0
-        for task in tasks:
+        for task in self.tasks:
             if task.processor != None:
                 ix = task.processor.index
                 self.processor_time_taken[i] += task.time_taken
                 self.target_progress += task.length
 
-    def evaluate_processor_time_taken():
+    def evaluate_processor_time_taken(self):
         for time_taken in self.processor_time_taken:
             if time_taken >= self.deadline:
                 return False
             return True 
 
-    def evaluate_task_target():
+    def evaluate_task_target(self):
         if self.target_progress >= self.target:
-            return true
-        return false
+            return True
+        return False
 
-    def write_goal_state():
+    def write_goal_state(self):
         self.assignments = ""
         for task in self.tasks:
             self.assignments += (task.processor.index + " ")
 
         print self.assignments
 
-    def evaluate_state():
+    def evaluate_state(self):
         assert(self.processor_time_taken != None)
-        if evaluate_processor_time_taken() and evaluate_task_target():
+        if self.evaluate_processor_time_taken() and self.evaluate_task_target():
             self.goal_state = True
-            write_goal_state()
+            return self.goal_state
 
     def __str__(self):
         print_str = "\nProcessors\n"
 
         for processor in self.processors:
-            print_str += ("Processor " + str(processor.index) + " Speed: " + str(processor.speed) + "\n")
+            print_str += str(processor)
 
         print_str += "\nTasks\n"
 
-        for i, task in enumerate(self.tasks):
-            print_str += ("Task " + str(i) + " Length: " + str(task.length)
-                          + " Assigned to Processor: " + str(task.processor) + "\n")
+        for task in self.tasks:
+            print_str += str(task)
 
         return print_str
 
