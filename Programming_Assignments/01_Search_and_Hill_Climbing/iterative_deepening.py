a9aa7481bb74178c95780dec29120d3b98a8f8b5
Mark <mark.j.xavier@gmail.com>
2019-02-21 15:48:03 -0500

    Managed to get iterative deepening working for input 1

--- Programming_Assignments/01_Search_and_Hill_Climbing/iterative_deepening.py
+++ Programming_Assignments/01_Search_and_Hill_Climbing/iterative_deepening.py
@@ -1,6 +1,7 @@
 
 import sys
 import argparse
+import copy
 
 from state import State
 
@@ -9,7 +10,41 @@ def iterative_deepening(file_path):
     """
 
     initial_state = State.load_from_file(file_path)
-    print initial_state
+
+    for depth in xrange(initial_state.minimum_tasks_needed, len(initial_state.tasks) + 1):
+        state = depth_limited_search(initial_state, depth, 0)
+
+def depth_limited_search(state, depth, current_task):
+    """
+    """
+
+    # print state
+    if depth == 0:
+        return
+
+    for processor in state.processors:
+        tasks = copy.deepcopy(state.tasks)
+        if processor == None:
+            tasks[current_task].assign_processor(processor)
+            new_state = State(state.processors, tasks, state.deadline, state.target)
+            if(new_state.goal_state):
+                new_state.write_goal_state()
+                return new_state
+            else:
+                depth_limited_search(new_state, depth-1, current_task+1)   
+        else:
+            task_length = state.tasks[current_task].length
+            processor_time = state.processor_time_taken[processor.index]
+
+            if ((task_length / processor.speed) + processor_time) < state.deadline:
+                tasks[current_task].assign_processor(processor)
+                new_state = State(state.processors, tasks, state.deadline, state.target)
+                if(new_state.goal_state):
+                    new_state.write_goal_state()
+                    return new_state
+                else:
+                    depth_limited_search(new_state, depth-1, current_task+1)
+
 
 def main():
     """
--- Programming_Assignments/01_Search_and_Hill_Climbing/state.py
+++ Programming_Assignments/01_Search_and_Hill_Climbing/state.py
@@ -1,32 +1,47 @@
 
 class Processor:
+    """
+    The Processor Class
+
+    This class serves as a wrapper for the processor speeds given in the input.  This is mainly
+    created to ease printing.
+    """
 
     def __init__(self, index, speed):
         self.index = index
         self.speed = speed
 
     def __str__(self):
-        return "Processor " + str(self.index) + " Speed: " + str(self.speed) + "\n"
+        return "Processor " + str(self.index) + " Speed: " + str(self.speed)
 
 
 class Task:
+    """
+    The Task Class
+
+    This class serves as a wrapper for the task lengths given, and provides an interface for
+    assigning tasks to processors.
+    """
 
     def __init__(self, index, length):
         self.index = index
         self.length = length
         self.processor = None
+        self.time_taken = 0
 
     def assign_processor(self, processor):
         self.processor = processor
-        self.time_taken = self.length / self.processor.speed
+        if processor != None:
+            self.time_taken = self.length / self.processor.speed
+        else:
+            self.time_taken = 0
 
-    def evaluate_task_time(self):
-        if(self.processor != None):
-            return self.time_taken
+    def __lt__(self, other):
+        return self.length < other.length
 
     def __str__(self):
         return ("Task " + str(self.index) + " Length: " + str(self.length)
-                + " Assigned to " + str(self.processor) + "\n")
+                + " Assigned to " + str(self.processor))
 
 
 class State:
@@ -38,8 +53,8 @@ class State:
         self.target = target
         self.goal_state = False
         self.processor_time_taken = None
+        self.get_minimum_number_of_tasks_needed()
         self.compute_state()
-        self.evaluate_state()
 
     @staticmethod
     def read_file(file_path):
@@ -62,16 +77,17 @@ class State:
         for index, speed in enumerate(processor_speeds):
             processors.append(Processor(index, speed))
         
+        processors.append(None)
         return cls(processors, tasks, parameters[0], parameters[1])
-    
-    def compute_state(self):
-        self.processor_time_taken = [0 for i in xrange(len(self.processors))]
-        self.target_progress = 0
-        for task in self.tasks:
-            if task.processor != None:
-                ix = task.processor.index
-                self.processor_time_taken[i] += task.time_taken
-                self.target_progress += task.length
+
+    def get_minimum_number_of_tasks_needed(self):
+        tasks = sorted(self.tasks, reverse=True)
+        min_num = 0
+        current_index = 0
+        while min_num < self.target:
+            min_num += tasks[current_index].length
+            current_index += 1
+        self.minimum_tasks_needed = current_index
 
     def evaluate_processor_time_taken(self):
         for time_taken in self.processor_time_taken:
@@ -84,29 +100,44 @@ class State:
             return True
         return False
 
-    def write_goal_state(self):
-        self.assignments = ""
-        for task in self.tasks:
-            self.assignments += (task.processor.index + " ")
-
-        print self.assignments
-
     def evaluate_state(self):
         assert(self.processor_time_taken != None)
         if self.evaluate_processor_time_taken() and self.evaluate_task_target():
             self.goal_state = True
         return self.goal_state
+    
+    def compute_state(self):
+        self.processor_time_taken = [0 for i in xrange(len(self.processors))]
+        self.target_progress = 0
+        for task in self.tasks:
+            if task.processor != None:
+                ix = task.processor.index
+                self.processor_time_taken[ix] += task.time_taken
+                self.target_progress += task.length
+        self.evaluate_state()
+
+    def write_goal_state(self):
+        self.assignments = ""
+        for task in self.tasks:
+            if task.processor != None:
+                self.assignments += (str(task.processor.index + 1) + " ")
+            else:
+                self.assignments += "0 "
+
+        print self.assignments
 
     def __str__(self):
         print_str = "\nProcessors\n"
 
         for processor in self.processors:
-            print_str += str(processor)
+            print_str += (str(processor) + "\n")
 
         print_str += "\nTasks\n"
 
         for task in self.tasks:
-            print_str += str(task)
+            print_str += (str(task) + "\n")
+
+        print_str += "\nParameters\n"
+        print_str += ("Deadline: " + str(self.deadline) + "\nTarget: " + str(self.target))
 
         return print_str
-
