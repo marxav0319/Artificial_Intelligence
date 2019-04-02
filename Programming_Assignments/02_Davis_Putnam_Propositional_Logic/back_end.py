409ffae12ede17ef3f2df71675bf95c07394ccd5
Mark <mark.j.xavier@gmail.com>
2019-04-02 13:33:52 -0400

    Backend done

---
+++ Programming_Assignments/02_Davis_Putnam_Propositional_Logic/back_end.py
@@ -0,0 +1,45 @@
+INFILE = r'temp_outputs/davis_putnam_output.txt'
+ASSIGN_ATOM = 'A'
+VALUE_ATOM = 'V'
+
+def read_input_file():
+    """
+    """
+    true_atoms = []
+    translations = []
+    switch = False
+    f = open(INFILE)
+    for line in f:
+        l = line.strip().split(" ")
+        if l[0] == '0':
+            switch = True
+            continue
+        elif switch == False and l[1] == 'T':
+            true_atoms.append(l[0])
+        else:
+            if l[0] in true_atoms and l[1] == ASSIGN_ATOM:
+                translations.append(l)
+    f.close()
+    return translations
+
+def print_translations(translations):
+    """
+    """
+    print '\nAssignments:'
+    for t in translations:
+        print "R%s = R%s at Time: %s" % (t[2], t[3], t[4])
+    return
+
+def main():
+    """
+    """
+    translations = read_input_file()
+    if len(translations) == 0:
+        print '\nNo Solution'
+    else:
+        print_translations(translations)
+
+    return
+
+if __name__ == '__main__':
+    main()
