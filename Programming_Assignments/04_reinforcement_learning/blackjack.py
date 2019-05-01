5803422d11d8893c17eee08654603b212c61527c
Mark <mark.j.xavier@gmail.com>
2019-05-01 15:58:12 -0400

    More work on programming assignment 4

--- Programming_Assignments/04_reinforcement_learning/blackjack.py
+++ Programming_Assignments/04_reinforcement_learning/blackjack.py
@@ -2,6 +2,40 @@
 """
 
 import argparse
+import random
+
+class Player:
+    
+    def __init__(self, u_target):
+        """
+        """
+
+        self.u_target = u_target
+        self.score = 0
+        self.drew_last_turn = 1
+        self.lost_game = False
+
+    def draw(self, card):
+        """
+        """
+
+        self.score += card
+        if self.score > self.u_target:
+            self.lost_game = True
+
+    def stayed_last_turn(self):
+        """
+        """
+
+        return False if self.drew_last_turn else True
+
+    def clear(self):
+        """
+        """
+
+        self.score = 0
+        self.drew_last_turn = 1
+        self.lost_game = False
 
 class Blackjack:
 
@@ -33,8 +67,9 @@ class Blackjack:
 
         # Variables defined for the script
         self.game_over = False
-        self.p1_score = 0
-        self.p2_score = 0
+        self.player_1 = Player(u_target)
+        self.player_2 = Player(u_target)
+        self.moves = []
 
     def _construct_array(self):
         """
@@ -58,21 +93,109 @@ class Blackjack:
             empty_list.append(temp_1)
         return empty_list
 
+    def _sample(self):
+        """
+        """
+
+        prob = 1/float(self.k + 1)
+        probs = [(prob * i) + prob for i in xrange(self.k + 1)]
+        x = random.random()
+        for i in xrange(self.k + 1):
+            if x < probs[i]:
+                return i
+
+    def draw(self):
+        """
+        """
+
+        return self._sample()
+
+    def add_move(p1, p2, cards_drawn):
+        """
+        """
+
+        self.moves.append([p1.score, p2.score, p1.drew_last_turn, cards_drawn])
+
+    def player_plays(self, player, other):
+        """
+        """
+
+        card = self.draw()
+        self.add_move(player, other, card)
+        player.draw(card)
+
+        if player.lost_game:
+            self.game_over = True
+
+        if card == 0:
+            player.drew_last_turn = 0
+
+    def check_no_play_made(self):
+        """
+        """
+
+        return self.player_1.stayed_last_turn() and self.player_2.stayed_last_turn()
+
     def play_game(self):
         """
         """
 
         while(not self.game_over):
-            draw()
-            continue
+            if self.check_no_play_made():
+                self.game_over = True
+            else:
+                self.player_plays(self.player_1, self.player_2)
+                if self.check_no_play_made():
+                    self.game_over = True
+                else:
+                    self.player_plays(self.player_2, self.player_1)
+
+    def determine_winner(self):
+        """
+        """
+
+        if not self.player_1.lost_game and not self.player_2.lost_game:
+            if self.player_1.score < self.player_2.score:
+                self.player_1.lost_game = True
+            else:
+                self.player_2.lost_game = True
+
+    def update(self):
+        """
+        """
+
+        p1_array = self.win_count
+        p2_array = self.lose_count
+        self.determine_winner()
+        if self.player_1.lost_game:
+            p1_array = self.lose_count
+            p2_array = self.win_count
+
+        current_player = 1
+        for element in self.moves:
+            if current_player == 1:
+                p1_array[element[0], element[1], element[2], element[3]] += 1
+            else:
+                p2_array[element[0], element[1], element[2], element[3]] += 1
+            current_player = 2 if current_player == 1 else 1
+
+    def clear(self):
+        """
+        """
+
+        self.player_1.clear()
+        self.player_2.clear()
+        self.game_over = False
+        self.moves = []
 
     def run_simulation(self):
         """
         """
 
         for game in xrange(self.n_games):
-            continue
-
+            self.play_game()
+            self.update()
+            self.clear()
         return
 
 def driver():
